3568fb98f274f4c2615e1f446aa13925f891c4a4
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-01-31 09:28:12 +0100

    Update artificial-intelligence.js

--- plugins/artificial-intelligence.js
+++ plugins/artificial-intelligence.js
@@ -359,3 +359,34 @@ command(
         }
     }
 );
+command(
+    {
+        pattern: "metaai",
+        desc: "response from meta ai",
+        fromMe: true,
+        type: "ai",
+    },
+    async (message, match) => {
+        await message.react("⏳️");
+        try {
+            
+            let query = match?.trim() || message.reply_message?.text;
+            const name = message.pushName;
+
+            if (!query) {
+                return await message.send(`Hey ${name}, how can I help you?`);
+            }
+
+            const apiUrl = `https://apii.ambalzz.biz.id/api/openai/meta-ai?ask=${encodeURIComponent(query)}`;
+            const resp = await getJson(apiUrl);
+            const res = resp?.r?.meta || "No response from API.";
+
+            await message.send(res);
+            await message.react("");
+        } catch (error) {
+            await message.send("An error occurred while processing your request.");
+            await message.react("❌️");
+            console.error(error);
+        }
+    }
+);
