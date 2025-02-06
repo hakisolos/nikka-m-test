48eef661e15cf5a13caf94dff1a8321d3ce12a64
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-02-06 09:40:06 +0100

    GOD DID

--- DB/store.js
+++ DB/store.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { isJidBroadcast, isJidGroup, isJidNewsletter } = require('baileys');
+const { isJidBroadcast, isJidGroup, isJidNewsletter } = require('haki-baileys');
 
 const storeDir = path.join(process.cwd(), 'store');
 
