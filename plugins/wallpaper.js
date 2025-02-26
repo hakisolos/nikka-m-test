4aa6f646dfdab0d5e68eb376419cf8f1dc0f0d23
H4KI XER <hakiisdepressed@gmail.com>
2025-02-26 11:02:20 -0800

    GOD DID

---
+++ plugins/wallpaper.js
@@ -0,0 +1,382 @@
+const { command, isPrivate } = require("../lib")
+
+command(
+    {
+        pattern: "abstract",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/abstract?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "anime",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/anime?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "aesthetic",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/aesthetic?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "akira",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/akira?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "anna",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/anna?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "asuna",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/asuna?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "animals",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/animals?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "avengers",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/avengers?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "books",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/books?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "babies",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/babies?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "bokeh",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/bokeh?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "birds",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/birds?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "bike",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/bike?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "bts",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/bts?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "blackpink",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/blackpink?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "boruto",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/boruto?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "comics",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/comics?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "couple-dps",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/couple-dps?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "creatures",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/creatures?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
+command(
+    {
+        pattern: "ronaldo",
+        desc: "wallpapaer",
+        type: "wallpaper",
+        fromMe: isPrivate,
+    },
+    async (message, m) => {
+        try{
+            
+            await message.sendFromUrl("https://api.nexoracle.com/wallpapers/christiano-ronaldo?apikey=free_key@maher_apis", {caption: "> POWERED BY NIKKA TECH"})
+            await message.react("")
+
+        } catch(error){
+            await m.send(error)
+        }
+    }
+)
+
